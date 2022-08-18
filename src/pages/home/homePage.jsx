442ecade40f8c586fd7962cf575b50d7a82db18b
Iago Puzer <iagopuzer@hotmail.com>
2022-08-18 01:35:44 -0300

    Ajustes de renderização e avanços no projeto

--- src/pages/home/homePage.jsx
+++ src/pages/home/homePage.jsx
@@ -1,46 +1,13 @@
 import {React ,useCallback, useState, useEffect } from 'react';
 import './homePage.css'
 import { useNavigate } from 'react-router-dom';
-import { wordsList } from '../../data/words';
 
 const Home = () => {
   const navigate = useNavigate()
-  const [words, setWords] = useState(wordsList)
-
-  const [pickedWords, setPickedWords] = useState('')
-  const [pickerCategory, setPickerCategory] = useState('')
-  const [letters, setLetters] = useState([])
-
-  const pickWordAndCategory = () => {
-    //selecionando uma categoria aletória
-    const categories = Object.keys(words)
-    const category = categories[Math.floor(Math.random() * Object.keys(categories).length)]
-  
-
-    //selecionando uma palavra aletória dentro da categoria
-    const word = words[category][Math.floor(Math.random() *  words[category].length)]
-    
-
-    return {word, category}
-  }
 
   const handleStartGame = () => {
     //navegando para a page game
     navigate('/inGame')
-
-    //selecionando a categoria e a palavra para inicar o jogo
-    const{word, category} = pickWordAndCategory()
-    console.log (word, category)
-
-    //separando a palavra por letras
-      let wordLtters = word.split('')
-      wordLtters = wordLtters.map((l) => l.toLowerCase())
-      console.log (wordLtters)
-
-    //fill states palavra
-    setPickedWords(word)
-    setPickerCategory(category)
-    setLetters(letters)
   }
 
 
--- src/pages/inGame/Game.jsx
+++ src/pages/inGame/Game.jsx
@@ -1,46 +1,107 @@
-import {React ,useCallback, useState, useEffect } from 'react';
+import {React ,useCallback, useState, useEffect, useRef } from 'react';
 import './Game.css'
 import { wordsList } from '../../data/words';
 import {useNavigate} from 'react-router-dom';
 
+
+
 const InGame = () => {
-    const [words, setWords] = useState(wordsList)
     const navigate = useNavigate ()
 
+    const [words, setWords] = useState(wordsList)
     const [pickedWords, setPickedWords] = useState('')
-    const [pickerCategory, setPickerCategory] = useState('')
+    const [pickedCategory, setPickedCategory] = useState('')
     const [letters, setLetters] = useState([])
 
-    const endGame = () => {
-        navigate ('/endGame')
+    const [guessedLetters, setGuessedLetters] = useState([])
+    const [wrongLetters, setWrongLetters] = useState([])
+    const [guesses, setGuesses] = useState(5)
+    const [score, setScore] = useState(0)
+
+    const [letter, setLetter] = useState('')
+    const letterInputRef = useRef(null)
+
+    const pickWordAndCategory = () => {
+        //selecionando uma categoria aletória
+        const categories = Object.keys(words)
+        const category = categories[Math.floor(Math.random() * Object.keys(categories).length)]
+      
+        //selecionando uma palavra aletória dentro da categoria
+        const word = words[category][Math.floor(Math.random() * words[category].length)]
+        
+        return {word, category}
+      }
+
+      //iniciar o game quando a pagina renderizar
+      useEffect (() => {
+          
+        //selecionando a categoria e a palavra para inicar o jogo
+        const{word, category} = pickWordAndCategory()
+        
+        console.log (word, category)
+
+        //separando a palavra por letras
+        let wordLetters = word.split('')
+        wordLetters = wordLetters.map((l) => l.toLowerCase())
+        console.log(wordLetters)
+
+        // fill states palavra
+        setPickedWords(word)
+        setPickedCategory(category)
+        setLetters(wordLetters)
+        console.log ('entrou')
+      },[])
+
+      
+
+    const verifyLetter = (letter) => {
+        console.log (letter)
     }
 
-    const verifyLetter = () => {
+    //envio do formulario ao selecionar escolher a letra para jogar
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        verifyLetter(letter)
 
+        setLetter('')
+
+        letterInputRef.current.focus()
     }
 
   return (
     <div className='game'>
-        <p className='point'>Pontuação: 000</p>
-        <h1 className=''>Adivinhe a palavra</h1>
-        <h3 className='tip'>Dica da palavra: Dica...</h3>
+        <p className='points'>Pontuação: {score}</p>
+        <h1>Adivinhe a palavra</h1>
+        <h3 className='tip'>Dica da palavra: <span>{pickedCategory}</span> </h3>
+        <p>Você ainda tem {guesses} tentativas</p>
 
-        <div className="word-container">
-            <span className = 'letter'>A</span>
-            <span className = 'blankSquare'></span>       
+        <div className="wordContainer">
+             {letters.map((letter,i)=>(
+                guessedLetters.includes(letter) ? 
+                    (<span key = {i} className = 'letter'>{letter}</span>)
+                 : (<span key = {i} className = 'blankSquare'></span>)
+             ))}    
         </div>  
 
         <div className="letterContainer">
             <p>Tente adivinhar a letra</p>
-            <form>
-                <input type="text" name='letter' maxLength='1' required/>
-                <button>Jogar!</button>  
+            <form onSubmit={handleSubmit}>
+                <input type="text"
+                 name='letter' 
+                 maxLength='1' 
+                 required 
+                 ref = {letterInputRef} 
+                 value = {letter} 
+                 onChange={e => setLetter(e.target.value)}/>
+                <button type='submit'>Jogar!</button>  
             </form>    
         </div>
 
-        <div className="wrondLettersContainers">
-        <p>Letras utilizadas</p>
-        <span>a,</span>
+        <div className="wordLettersContainers">
+        <p>Letras utilizadas:</p>
+        {wrongLetters.map((letter, i)=>(
+            <span key = {i}>{letter},</span>
+        ))}
         </div>      
     </div>
   )
